200c284400251cf216e6e0d7b0bb4ea866ac233b
Mar <118215891+morakoshka@users.noreply.github.com>
2023-11-19 20:45:24 -0600

    Add files via upload

--- consorcio/tabla.php
+++ consorcio/tabla.php
@@ -1,19 +1,30 @@
-<style>
-    table{
-        border:1px solid black;
-    }
-    td{
-        border:1px solid black;
-    }
-</style>
- 
+<!DOCTYPE html>
+<html lang="en">
 <?php
- include 'functions.php';
+    include_once '../static/head.php';
+    include 'functions.php';
 ?>
+<body>
+<?php include_once '../static/navbar.php'; ?>
+<div id="page-container">
 
-<html>
-    <body>
-        <a  href="insert.php"><button class="button">Agregar registro</button></a>
-        <?php echo select_all();?>
-    </body>
+    <div id="content-wrap">
+        <div class="jumbotron">
+        <div class="container text-center">
+            <h1>CATÁLOGO</h1>      
+            <p>para Instituciones de Educación a nivel Superior</p>
+        </div>
+        </div>
+        
+        <div class="text-center">    
+            <h1>Consorcio</h1><br>
+            <a href="insert.php"><button class="button" role = "button">Agregar registro</button></a>
+            <div id="tabla">
+            <?php echo select_all(); ?>
+            </div>
+        </div><br><br>
+    </div>
+    <?php include_once '../static/footer.php' ?>
+</div>
+</body>
 </html>
