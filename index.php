49ee79bbaf9b8f94bc8224701517af4aa7ce7285
176608 <151085642+176608@users.noreply.github.com>
2023-11-20 19:58:01 -0700

    Consorcio
    actualizacion consorcio.php

--- index.php
+++ index.php
@@ -52,7 +52,6 @@ mysqli_close($conn);
 
 <script>
 new DataTable('#test');
-
 </script>
 
 
--- views/consorcio/consorcio.php
+++ views/consorcio/consorcio.php
@@ -1,6 +1,6 @@
 <?php
 //echo "Estoy en consorcio.php"; 
-
+include('../../_assets/conn.php');
 echo '';
 
 ?>
@@ -16,34 +16,51 @@ echo '';
 
   <div class="container-fluid mt-3">
     <h2>Sección 2</h2>
-    <table class="table table-dark table-hover">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colspan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </table>
+
+<?php
+    $query = "SELECT id_consorcio, Siglas, Nombre, Enlace FROM consorcio";
+    $resultado = mysqli_query($conn, $query);
+    if ($resultado) {
+    // Iniciar la tabla HTML
+    echo '<table class="table" id="consorcios" class="display" style="width:100%">
+            <thead>
+                <tr>
+                    <th scope="col">#ID</th>
+                    <th scope="col">Siglas</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Enlace</th>
+                    <th scope="col">Editar</th>
+                    <th scope="col">Eliminar</th>
+                </tr>
+            </thead>
+            <tbody>';
+    // Iterar sobre los resultados
+    while ($fila = mysqli_fetch_assoc($resultado)) {
+        echo '<tr>
+                <td>' . $fila['id_consorcio'] . '</td>
+                <td>' . $fila['Siglas'] . '</td>
+                <td>' . $fila['Nombre'] . '</td>
+                <td>' . $fila['Enlace'] . '</td>
+                <td> boton para editar </td>
+                <td> boton para borrar </td>
+            </tr>';
+    }
+    // Cerrar la tabla HTML
+    echo '</tbody>
+        </table>';
+    } else {
+        // Manejar el caso en que la consulta no fue exitosa
+        echo 'Error en la consulta: ' . mysqli_error($conn);
+    }
+
+    // Cerrar la conexión a la base de datos
+    mysqli_close($conn);
+?>
+
   </div>
 </div>
+
+
+<script>
+new DataTable('#consorcios');
+</script>
